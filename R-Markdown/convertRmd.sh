9e20d695f527f02e1d8c2a7f6791ea6d93f33bbe
Jon Atherton <jon.atherton@helsinki.fi>
2018-10-15 09:43:43 +0300

    moved file

--- R-Markdown/convertRmd.sh
+++ R-Markdown/convertRmd.sh
@@ -7,4 +7,4 @@
 Rscript -e "library(knitr); knit('R1-Introduction.Rmd')"
 pandoc R1-Introduction.md > R1-Introduction.html
 Rscript -e "library(rmd2jupyter); rmd2jupyter('R1-Introduction.Rmd')"
-git mv R1-Introduction.Rmd ../R1-Introduction.Rmd
+git mv R1-Introduction.ipynb ../R1-Introduction.ipynb
